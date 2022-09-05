ac1b07273db51aa7e428aab5ebe2b9694b493e91
StrummingDown <mylove0330@nate.com>
2022-09-05 23:57:12 +0900

    [Add] 아이디 찾기시 모달창 적용중

--- client/src/pages/FindId.js
+++ client/src/pages/FindId.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   CheckText,
   Container,
@@ -11,28 +11,51 @@ import {
 } from "../css/FindId";
 import axios from "axios";
 import { ResultId } from "../components/ResultId";
+import { FindIdModal } from "../modal/FindIdModal";
 
 export const FindId = () => {
   const [certifyNumber, setCertifyNumber] = useState(0);
+  const [inputCertifyNumber, setInputCertiyNumber] = useState(-1);
   const [checkNumber, setCheckNumber] = useState(false);
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
   const [idResult, setIdResult] = useState(false);
   const [userId, setUserId] = useState("");
+  const [modalOpen, setModalOpen] = useState(false);
 
-  const sendCertifyNumber = async () => {
-    alert("인증번호를 전송했습니다.");
-    const { data } = await axios.post("http://localhost:4000/users/certify", { phone });
-    setCertifyNumber(data);
+  // useEffect(() => {
+  //   checkCertifyNumber();
+  // }, [checkNumber]);
+  const openModal = () => {
+    console.log("확인", checkNumber);
+    if (!checkNumber) {
+      setModalOpen(true, () => {
+        checkNumber();
+      });
+    }
   };
-  const checkCertifyNumber = () => {};
-  const inputNumber = (e) => {
-    console.log(certifyNumber, e.target.value);
-    if (certifyNumber === parseInt(e.target.value)) {
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+  const sendCertifyNumber = async () => {
+    if (phone.length < 11) {
+      alert("핸드폰 번호를 정확하게 입력해주세요.");
+    } else {
+      alert("인증번호를 전송했습니다.");
+      const { data } = await axios.post("http://localhost:4000/users/certify", { phone });
+      setCertifyNumber(data);
+    }
+  };
+  const checkCertifyNumber = () => {
+    if (certifyNumber === inputCertifyNumber) {
       setCheckNumber(true);
     } else {
       setCheckNumber(false);
     }
+  };
+  const inputNumber = (e) => {
+    console.log("여기", certifyNumber, inputCertifyNumber.length, phone.length);
+    setInputCertiyNumber(parseInt(e.target.value));
   };
 
   const findId = async () => {
@@ -42,6 +65,12 @@ export const FindId = () => {
     } = await axios.post("http://localhost:4000/users/findId", { email });
     setUserId(userId);
     setIdResult(true);
+  };
+
+  const checkEmail = (e) => {
+    var regExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+
+    console.log("이메일 유효성 검사 :: ", regExp.test(e.target.value));
   };
   return (
     <Container>
@@ -55,21 +84,43 @@ export const FindId = () => {
       ) : (
         <FindIdContainer>
           <Title>아이디 찾기</Title>
+
+          <FindIdModal open={modalOpen} close={closeModal} header="Modal heading">
+            인증번호가 일치하지 않습니다 !
+          </FindIdModal>
           <FindIdText>E-mail</FindIdText>
-          <FindIdInput onChange={(e) => setEmail(e.target.value)} placeholder="Please Enter your E-mail" />
+          <FindIdInput
+            onBlur={checkEmail}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Please Enter your E-mail"
+          />
           <FindIdText>핸드폰 번호</FindIdText>
           <FindIdInput onChange={(e) => setPhone(e.target.value)} placeholder="Please Enter your Phone" />
           <FindIdBtn onClick={sendCertifyNumber}>인증번호 전송</FindIdBtn>
           <FindIdText>인증번호</FindIdText>
           <FindIdInput onChange={inputNumber} placeholder="Please Enter Number" />
-          <FindIdBtn onClick={checkCertifyNumber}>인증번호 확인</FindIdBtn>
+          <FindIdBtn
+            onClick={() => {
+              checkCertifyNumber();
+
+              openModal();
+              // }
+            }}
+          >
+            인증번호 확인
+          </FindIdBtn>
           <div style={{ textAlign: "center" }}>
-            {!checkNumber ? (
-              <CheckText>인증 번호가 일치하지 않습니다!</CheckText>
-            ) : (
-              <CorrectText>인증 번호가 일치합니다.</CorrectText>
-            )}
-            <FindIdBtn onClick={findId}>아이디 찾기</FindIdBtn>
+            {/* {!checkNumber && inputCertifyNumber !== -1 ? <CheckText>인증 번호가 일치하지 않습니다!</CheckText> : ""} */}
+            <FindIdBtn
+              disabled={
+                phone.length === 11 && String(inputCertifyNumber).length === 6 && String(certifyNumber).length === 6
+                  ? false
+                  : true
+              }
+              onClick={findId}
+            >
+              아이디 찾기
+            </FindIdBtn>
           </div>
         </FindIdContainer>
       )}
