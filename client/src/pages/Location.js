c2d967006217e9acd4f30e674252258683b07b0c
StrummingDown <mylove0330@nate.com>
2022-09-13 23:53:18 +0900

    [Update] Map

--- client/src/pages/Location.js
+++ client/src/pages/Location.js
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+const { kakao } = window;
 
 export const Location = () => {
-  return <div>Location</div>;
+  useEffect(() => {
+    const container = document.getElementById("myMap");
+    const options = {
+      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3,
+    };
+    const map = new kakao.maps.Map(container, options);
+  }, []);
+
+  return (
+    <div>
+      <h2 style={{ textAlign: "center" }}>내 위치</h2>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <div
+          id="myMap"
+          style={{
+            width: "500px",
+            height: "80vh",
+          }}
+        ></div>
+      </div>
+    </div>
+  );
 };
